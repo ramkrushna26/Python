1507ecccee2a2319ee93244e9cf87148c2f1e404
ramkrushna26 <ramkrushna.bolewar1@gmail.com>
2020-05-31 00:09:44 +0530

    added files

--- django/django_project/blog/views.py
+++ django/django_project/blog/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': "Batman",
+        'title': 'blog post 1',
+        'content': 'my blog post first',
+        'date_posted': '24-June-2020'
+    },
+    {
+        'author': "Iron Man",
+        'title': 'blog post 2',
+        'content': 'my blog post second',
+        'date_posted': '24-Auguest-2020'
+    }
+]
 
 def home(request):
-    return HttpResponse("<h1> Blog Home </h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title' : 'About'})
