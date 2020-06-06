a919cb4f33441e89d6a40763a8ed6f156cea5b4a
ramkrushna26 <ramkrushna.bolewar1@gmail.com>
2020-06-06 16:47:11 +0530

    modified files

--- django/django_project/django_project/settings.py
+++ django/django_project/django_project/settings.py
@@ -132,7 +132,7 @@ LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_POST = 587
 EMAIL_USE_TLS = True
+EMAIL_POST = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
